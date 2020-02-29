a09506ec9d07058c163f9437a525870088f6d635
Mustapha Rachidi <murachid@e1r1p8.1337.ma>
2020-02-29 01:34:37 +0100

    kjsbdvkjs

--- ft_printf.h
+++ ft_printf.h
@@ -13,9 +13,9 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <stdlib.h>
-#include <stdarg.h>
-#include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include <unistd.h>
 
 typedef struct	s_flags
 {
